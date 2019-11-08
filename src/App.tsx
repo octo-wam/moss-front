7fcbcca20683b8834c0c4e65d34220174570d50b
Annaëlle Sagot <asagot@octo.com>
2019-11-08 16:11:08 +0100

    Add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
+import { Header } from "./components/Header/Header";
 import { QuestionsList } from "./components/QuestionsList/QuestionsList";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Switch>
+        <Header/>
+        <Switch>
         <Route exact path="/questions" render={() => <QuestionsList />} />
         <Redirect to="/questions" />
-      </Switch>
+        </Switch>
     </div>
   );
 };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.scss";
+import octoLogo from "./octo-logo.svg"
+import React, { useEffect, useState } from "react";
+
+export interface HeaderProps {}
+
+export const Header: React.FC<HeaderProps> = () => {
+    return (
+        <div className="header">
+            <span>M</span>
+            <img src={octoLogo} alt="O"/>
+            <span>SS</span>
+        </div>
+    );
+};
