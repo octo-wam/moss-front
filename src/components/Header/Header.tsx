96cdd2ec1168349b10de15165881afe96780ec64
Antoine <antoine.chalifour@gmail.com>
2019-11-08 17:32:09 +0100

    submit votes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,17 @@
-import octoLogo from "./octo-logo.svg"
-import React, { useEffect, useState } from "react";
+import octoLogo from "./octo-logo.svg";
+import React from "react";
 
 export interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
-    return (
-        <div className="header">
-            <div className="header-acronym">
-                <span>M</span>
-                <img src={octoLogo} alt="O"/>
-                <span>SS</span>
-            </div>
-            <div className="header-def">
-                Massive Onlive Simple Sociocraty
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="header-acronym">
+        <span>M</span>
+        <img src={octoLogo} alt="O" />
+        <span>SS</span>
+      </div>
+      <div className="header-def">Massive Onlive Simple Sociocraty</div>
+    </div>
+  );
 };
--- src/components/QuestionDetail/QuestionDetail.tsx
+++ src/components/QuestionDetail/QuestionDetail.tsx
@@ -1,50 +1,69 @@
 import React, { useEffect, useState } from "react";
-import { Question } from "../../models";
+import { Question, Answer } from "../../models";
 import { useServices } from "../ServicesProvider";
 import moment from "moment";
 
-export interface QuestionDetailProps { id: string }
-
-const question: Question = {
-  id: "un id",
-  title: "Quelle est la couleur du cheval blanc d'Henri IV ?",
-  description: "une description",
-  endingDate: "01-12-2019",
-  answers: []
+export interface QuestionDetailProps {
+  id: string;
 }
 
 export const QuestionDetail: React.FC<QuestionDetailProps> = ({ id }) => {
   const services = useServices();
-  const [question, setQuestion] = useState<Question|null>(null);
+  const [currentAnswerId, setCurrentAnswerId] = useState<Answer | null>(null);
+  const [isFormDisabled, setFormDisabled] = useState(false);
+  const [question, setQuestion] = useState<Question | null>(null);
 
   useEffect(() => {
     services.question.fetchQuestion(id).then(setQuestion);
   }, [services.question, id]);
 
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    await services.vote.createVoteByQuestionid(id, currentAnswerId!.id);
+
+    setFormDisabled(true);
+    setCurrentAnswerId(null);
+  }
+
   if (question === null) {
-    return (
-      <p>Loading...</p>
-    );
+    return <p>Loading...</p>;
   }
   return (
     <div className="question-detail">
-      <header>
-        <h1 className="title">{ question.title }</h1>
-        <p className="description">{ question.description }</p>
-        <p className="ending-date">Expires on {moment(question.endingDate).format('LLL')}</p>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <header>
+          <h1 className="title">{question.title}</h1>
+          <p className="description">{question.description}</p>
+          <p className="ending-date">
+            Expires on {moment(question.endingDate).format("LLL")}
+          </p>
+        </header>
 
-      <ul>
-        {question.answers.map(answer => <li key={ answer.id }>
-          <input type="radio" name="answer" id={ answer.id } value={ answer.id } />
-          <label htmlFor={ answer.id }>{ answer.title }</label>
-        </li>)}
-      </ul>
+        <ul>
+          {question.answers.map(answer => (
+            <li key={answer.id}>
+              <input
+                checked={currentAnswerId === answer}
+                onChange={() => setCurrentAnswerId(answer)}
+                disabled={isFormDisabled}
+                type="radio"
+                name="answer"
+                id={answer.id}
+                value={answer.id}
+              />
+              <label htmlFor={answer.id}>{answer.title}</label>
+            </li>
+          ))}
+        </ul>
 
-      <footer>
-        <button type="submit">Valider</button>
-        <button type="button">Voir les résultats</button>
-      </footer>
+        <footer>
+          <button disabled={!currentAnswerId} type="submit">
+            Valider
+          </button>
+          <button type="button">Voir les résultats</button>
+        </footer>
+      </form>
     </div>
   );
 };
