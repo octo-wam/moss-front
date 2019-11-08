4071922c2153a5b77b5c65d1d0e5c6ec435dd088
Annaëlle Sagot <asagot@octo.com>
2019-11-08 17:43:34 +0100

    Add MOSS def to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,9 +6,14 @@ export interface HeaderProps {}
 export const Header: React.FC<HeaderProps> = () => {
     return (
         <div className="header">
-            <span>M</span>
-            <img src={octoLogo} alt="O"/>
-            <span>SS</span>
+            <div className="header-acronym">
+                <span>M</span>
+                <img src={octoLogo} alt="O"/>
+                <span>SS</span>
+            </div>
+            <div className="header-def">
+                Massive Onlive Simple Sociocraty
+            </div>
         </div>
     );
 };
