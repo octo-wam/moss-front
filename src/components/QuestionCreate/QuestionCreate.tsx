f194c5802d51dd6d032c0704cddb30ca2ad2ac25
pierrettebertrand <pierrette.bertrand@gmail.com>
2020-01-10 14:53:13 +0100

    remark1

--- src/components/QuestionCreate/QuestionCreate.tsx
+++ src/components/QuestionCreate/QuestionCreate.tsx
@@ -3,84 +3,9 @@ import { useHistory } from "react-router-dom";
 import uuid from "uuid/v4";
 
 import { useServices } from "../ServicesProvider";
+import { useQuestionCreateState } from "./hooks";
 
 export interface QuestionCreateProps {}
-
-const useQuestionCreateState = () => {
-  const [title, setTitle] = useState(""); // initial value
-  const [description, setDescription] = useState(""); // initial value
-  const [endingDate, setEndingDate] = useState(""); // initial value
-
-  const services = useServices();
-  const history = useHistory();
-  const [answers, setAnswers] = useState([
-    {
-      id: uuid(),
-      title: "",
-      description: '',
-    },
-    {
-      id: uuid(),
-      title: "",
-      description: '',
-    }
-  ]);
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    
-    services.question.createQuestion({ title, description, endingDate, answers }).then(() => history.push('/questions'));
-  }
-
-  function updateAnswerTitle(id: string, newTitle: string) {
-    setAnswers(answers =>
-      answers.map(answer => {
-        if (answer.id === id) {
-          return { ...answer, title: newTitle };
-        }
-
-        return answer;
-      })
-    );
-  }
-
-  function updateAnswerDescription(id: string, newDescription: string) {
-    setAnswers(answers =>
-      answers.map(answer => {
-        if (answer.id === id) {
-          return { ...answer, description: newDescription };
-        }
-
-        return answer;
-      })
-    );
-  }
-
-  function addAnswer() {
-    setAnswers(answers => [
-      ...answers,
-      {
-        id: uuid(),
-        title: "",
-        description: '',
-      }
-    ]);
-  }
-
-  return {
-    title,
-    setTitle,
-    description,
-    setDescription,
-    endingDate,
-    setEndingDate,
-    answers,
-    updateAnswerTitle,
-    updateAnswerDescription,
-    addAnswer,
-    handleSubmit
-  };
-};
 
 export const QuestionCreate: React.FC<QuestionCreateProps> = () => {
   const {
@@ -100,72 +25,87 @@ export const QuestionCreate: React.FC<QuestionCreateProps> = () => {
   return (
     <form onSubmit={handleSubmit}>
       <div className="field">
-        <label htmlFor="title">Question</label>
-        <input
-          placeholder="Intitulé de la question"
-          id="title"
-          type="text"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-        />
+        <label htmlFor="title">
+          Question
+          <input
+            placeholder="Que voulez-vous faire comme WAMEO ?  "
+            id="title"
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </label>
       </div>
 
       <div className="field">
-        <label htmlFor="description">Description</label>
-        <input
-          placeholder="Description de la question"
-          id="description"
-          type="textarea"
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-          className="question-description"
-        />
+        <label htmlFor="description">
+          Description
+          <input
+            placeholder="Nous sommes en train d’organiser un WAMEO - nous cherchons le meilleur plan !"
+            id="description"
+            type="textarea"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            className="question-description"
+          />
+        </label>
       </div>
 
       <div className="field">
-        <label htmlFor="endingDate">Date de fin</label>
-        <input
-          type="datetime-local"
-          id="endingDate"
-          name="meeting-time"
-          value={endingDate}
-          onChange={e => setEndingDate(e.target.value)}
-        />
+        <label htmlFor="endingDate">
+          Date de fin
+          <input
+            type="datetime-local"
+            id="endingDate"
+            name="meeting-time"
+            value={endingDate}
+            onChange={e => setEndingDate(e.target.value)}
+          />
+        </label>
       </div>
 
-    <div className="availableAnswers">
-      {answers.map(answer => (
-        <div className="answer">
-          <p>Réponse</p>
+      <div className="availableAnswers">
+        {answers.map(answer => (
+          <div className="answer">
+            <p>Réponse</p>
+            <div className="field">
+              <label htmlFor={`answer-title-${answer.id}`}>
+                Intitulé
+                <input
+                  placeholder="Aux arcs"
+                  id={`answer-title-${answer.id}`}
+                  type="text"
+                  value={answer.title}
+                  onChange={e => updateAnswerTitle(answer.id, e.target.value)}
+                />
+              </label>
+            </div>
 
-          <div className="field">
-            <label htmlFor={`answer-title-${answer.id}`}>Intitulé</label>
-            <input
-              placeholder="Label de la réponse"
-              id={`answer-title-${answer.id}`}
-              type="text"
-              value={answer.title}
-              onChange={e => updateAnswerTitle(answer.id, e.target.value)}
-            />
+            <div className="field">
+              <label htmlFor={`answer-description-${answer.id}`}>
+                Description
+                <input
+                  placeholder="Best place ever ❤️"
+                  id={`answer-description-${answer.id}`}
+                  type="textarea"
+                  value={answer.description}
+                  onChange={e =>
+                    updateAnswerDescription(answer.id, e.target.value)
+                  }
+                />
+              </label>
+            </div>
           </div>
-
-          <div className="field">
-            <label htmlFor={`answer-description-${answer.id}`}>Description</label>
-            <input
-              placeholder="Description de la réponse"
-              id={`answer-description-${answer.id}`}
-              type="textarea"
-              value={answer.description}
-              onChange={e => updateAnswerDescription(answer.id, e.target.value)}
-            />
-          </div>
-        </div>
-      ))}
-      <button type="button" className="add-answer" onClick={addAnswer}>
-        +
-      </button>
+        ))}
+        <button type="button" className="add-answer" onClick={addAnswer}>
+          +
+        </button>
       </div>
-      <div className="field"><button type="submit" className="submit-question">Créer le vote</button></div>
+      <div className="field">
+        <button type="submit" className="submit-question">
+          Créer le vote
+        </button>
+      </div>
     </form>
   );
 };
