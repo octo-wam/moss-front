41c0728ad5a888f2606567e7ed9fdc9cf419e9cb
Anne-Marie <amesteves@octo.com>
2020-08-06 11:34:31 +0200

    use prod or dev env url for login page url

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,11 +1,5 @@
 const OAUTH_KEY_STORAGE_KEY = "auth:token";
 
-const { NODE_ENV } = process.env
-if (!NODE_ENV) {
-  throw new Error('The NODE_ENV environment variable is required but was not specified.')
-}
-const isProdEnv = NODE_ENV === 'production';
-
 function getTokenFromUrl() {
   const searchParams = new URLSearchParams(document.location.hash);
 
@@ -36,9 +30,7 @@ export function getOauthTokenOrRedirect(): string {
   return "";
 }
 
-const AUTH_BASE_URL = isProdEnv ? process.env.REACT_APP_OTHER_BASE_URL : process.env.REACT_APP_LOCAL_BASE_URL;
-
-const LOGIN_PAGE_URL = `${AUTH_BASE_URL}/auth/google_oauth2`
+const LOGIN_PAGE_URL = `${process.env.REACT_APP_BASE_URL}/auth/google_oauth2`
 
 export function redirectToLoginPage() {
   window.location.href = `${LOGIN_PAGE_URL}?redirect_to=${window.location.href}`;
